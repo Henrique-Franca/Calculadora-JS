b5f91943ece3fc804d61f576217d895af0e730ff
HenriqueFranca <henriqsf@gmail.com>
2022-09-19 12:36:42 -0300

    clicando varias vezes no botão igual

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,6 +1,10 @@
 class CalcController{
 
     constructor(){
+        this._lastNumber = '';
+        this._lastOperator = '';
+
+
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -92,19 +96,44 @@ class CalcController{
 
     }
 
+    //Metodo para o resultado
+    getResult(){
+
+       // console.log("getResult", this._operation)
+
+        return eval(this._operation.join(""));
+    }
+
      //metodo para calcular
     calc(){
 
         let  last = '';
 
-        if(this._operation.length >3){
-            last = this._operation.pop();
-        }
+        this._lastOperator = this.getLastItem();
 
+       if(this._operation.length < 3){
+
+            let firstItem = this._operation[0];
+           this._operation = [firstItem, this._lastOperator, this._lastNumber];
+
+       }
+
+        // tratando o botão igual
+        if(this._operation.length >3){
+
+            last = this._operation.pop();
+            this._lastNumber= this.getResult();
+
+        }else if(this._operation.length == 3){
+            
+            this._lastNumber= this.getLastItem(false);
+
+        }
 
         
 
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
+
         //caso seja porcentagem 
         if(last == '%'){
 
@@ -117,20 +146,39 @@ class CalcController{
             if(last) this._operation.push(last);
         }
 
-        this.setLastNumberToDisplay();
+        this.setToDisplay();
     }
 
+    //metodo para clicar mais de uma vez no botao igual
+    getLastItem(isOperator = true){
+
+        let lastItem;
+        
+       for(let i  = this._operation.length - 1; i >=0; i--){
+        if(this.isOperator(this._operation[i]) == isOperator){
+            lastItem = this._operation[i];
+            break;
+        } 
+        
+       }
+
+       if(!lastItem){
+
+        lastItem = (isOperator) ? this._lastOperator :  this._lastNumber;
+
+
+       }
+
+
+       return lastItem;
+
+    }
 
     //metodo  para adicionar o ultimo numero no display
     setLastNumberToDisplay(){
 
-        let lastNumber;
-        for(let i = this._operation.length - 1; i >=0; i--){
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
-                break;
-            }
-        }
+        let lastNumber = this.getLastItem(false);
+       
 
         if(!lastNumber) lastNumber = 0;
 
