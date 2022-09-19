7e795a6e3247b47624e6846da2155a452e9b8cd9
HenriqueFranca <henriqsf@gmail.com>
2022-09-19 08:37:34 -0300

    cursor pointer

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -27,16 +27,33 @@ class CalcController{
 
     }
 
+    //metodo para eventos click e drag (tbm pode ser para masi eventos)
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false);
+        });
+
+    }
+
+
     //definindo buttons
     initButtonsEvent(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
         
         buttons.forEach((btn, index) =>{
-            btn.addEventListener('click',e =>{
+            this.addEventListenerAll(btn, "click drag",e =>{
                 console.log(btn.className.baseVal.replace("btn-","")); 
             });
-        })    
+
+            //mudando o cursor do mouse qunado passa pelo botão
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
+
+                btn.style.cursor = "pointer";
+
+            });
+        });    
         
         
         //buttons.addEventListener('click',e =>{
