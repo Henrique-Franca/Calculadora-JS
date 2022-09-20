a9a70d01fb96533dfdb39b742131700f0971850e
HenriqueFranca <henriqsf@gmail.com>
2022-09-20 09:14:07 -0300

    Evento de teclado

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -13,6 +13,7 @@ class CalcController{
         this._currentDate;
         this.initialize();
         this.initButtonsEvent();
+        this.initKeyboard();
 
     }
 
@@ -32,6 +33,58 @@ class CalcController{
 
         },10000)*/
 
+    }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e=>{
+            switch(e.key){
+
+                case 'Escape':
+                    this.clearAll();    
+                break;  
+    
+                case 'Backspace':
+                    this.clearEntry();    
+                break;  
+    
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+
+                    this.addOperation(e.key);
+                break;  
+                
+                case 'Enter':
+                    this.calc();
+                break;  
+                
+                case '.':
+                case ',':
+                        this.addDot(); 
+                break;  
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':  
+                
+                    this.addOperation(parseInt(e.key));
+    
+                break;
+    
+    
+               
+    
+            }
+        })
     }
 
     //metodo para eventos click e drag (tbm pode ser para masi eventos)
@@ -242,9 +295,10 @@ class CalcController{
     //programação do ponto
     addDot(){
 
-        if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
-
        let lastOperation = this.getLastOperation();
+
+       if(typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
+
        if(this.isOperator(lastOperation) || !lastOperation){
         this.pushOperation('0.');
        }else{
